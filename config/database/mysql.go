f5d90c29a6c5dbfc8763610d9239a68804e1dd06
Ahmad Fauzi <adzi.fauziahmad@gmail.com>
2024-12-29 21:02:17 +0700

    docker-compose and setup mysql

--- config/database/mysql.go
+++ config/database/mysql.go
@@ -3,7 +3,7 @@ package database
 import (
 	"cart-service/config"
 	"fmt"
-	"sync"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -15,34 +15,56 @@ type mysqlDatabase struct {
 }
 
 var (
-	once       sync.Once
+	// once       sync.Once
 	dbInstance *mysqlDatabase
 )
 
 func NewMysqlDatabase() Database {
 
-	once.Do(func() {
+	// once.Do(func() {
 
-		dbUsername := config.ENV.DB_USERNAME
-		dbPassword := config.ENV.DB_PASSWORD
-		dbName := config.ENV.DB_NAME
-		dbHost := config.ENV.DB_HOST
-		dbPort := config.ENV.DB_PORT
+	dbUsername := config.ENV.DB_USERNAME
+	dbPassword := config.ENV.DB_PASSWORD
+	dbName := config.ENV.DB_NAME
+	dbHost := config.ENV.DB_HOST
+	dbPort := config.ENV.DB_PORT
 
-		connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&loc=Local", dbUsername, dbPassword, dbHost, dbPort, dbName)
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&loc=Local", dbUsername, dbPassword, dbHost, dbPort, dbName)
 
-		db, err := gorm.Open(mysql.Open(connectionString), &gorm.Config{
+	var db *gorm.DB
+	var err error
+
+	// Retry connecting to the database up to 10 times
+	for i := 0; i < 10; i++ {
+		db, err = gorm.Open(mysql.Open(connectionString), &gorm.Config{
 			Logger: logger.Default.LogMode(logger.Info), // Enable logging for debugging
 		})
-
-		if err != nil {
-			panic("failed to connect database")
+		if err == nil {
+			break
 		}
+		fmt.Printf("Failed to connect to the database. Retrying in 2 seconds... (%d/10)\n", i+1)
+		time.Sleep(2 * time.Second)
+	}
 
-		dbInstance = &mysqlDatabase{Db: db}
-	})
+	if err != nil {
+		panic("failed to connect database after 10 attempts")
+	}
 
+	dbInstance = &mysqlDatabase{Db: db}
 	return dbInstance
+
+	// db, err := gorm.Open(mysql.Open(connectionString), &gorm.Config{
+	// 	Logger: logger.Default.LogMode(logger.Info), // Enable logging for debugging
+	// })
+
+	// if err != nil {
+	// 	panic("failed to connect database")
+	// }
+
+	// dbInstance = &mysqlDatabase{Db: db}
+	// // })
+
+	// return dbInstance
 }
 
 func (p *mysqlDatabase) GetDb() *gorm.DB {
